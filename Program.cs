321376559cd0cb58188e753ce17cb9893571298a
elpisofthelake <acharyasachin429@gmail.com>
2024-02-06 19:51:47 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 50; i < 70; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
