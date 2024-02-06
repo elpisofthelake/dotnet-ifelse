34c7b78496efd4795ea52625c35d1f6667d31792
elpisofthelake <acharyasachin429@gmail.com>
2024-02-06 20:06:03 +0545

    Mistakely uploaded for loop instead of if else. So re uploading it.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace ifElse
     {
         static void Main(string[] args)
         {
-            for (int i = 50; i < 70; i++)
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int.TryParse(age, out int Age);
+            int more = 18 - Age;
+            if(Age >= 18)
             {
-                Console.WriteLine(i);
+                Console.WriteLine("You can legally vote in Nepal");
+            }
+            else
+            {
+                Console.WriteLine("Sorry buddy! You still have to wait " + more + " more years.");
             }
         }
     }
